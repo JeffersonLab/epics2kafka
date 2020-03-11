03b0294bd74456d14e55c7c8999b6af8ec67a28b
ryans <ryans@jlab.org>
2020-03-11 08:06:17 -0400

    Initial commit

---
+++ src/main/java/org/jlab/kafka/CAConnectorSource.java
@@ -0,0 +1,72 @@
+package org.jlab.kafka;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.Task;
+import org.apache.kafka.connect.source.SourceConnector;
+
+import java.util.List;
+import java.util.Map;
+
+public class CAConnectorSource extends SourceConnector {
+
+    public static final String version = "0.0.0";
+
+    /**
+     * Start this Connector. This method will only be called on a clean Connector, i.e. it has
+     * either just been instantiated and initialized or {@link #stop()} has been invoked.
+     *
+     * @param props configuration settings
+     */
+    @Override
+    public void start(Map<String, String> props) {
+
+    }
+
+    /**
+     * Returns the Task implementation for this Connector.
+     */
+    @Override
+    public Class<? extends Task> taskClass() {
+        return CASourceTask.class;
+    }
+
+    /**
+     * Returns a set of configurations for Tasks based on the current configuration,
+     * producing at most count configurations.
+     *
+     * @param maxTasks maximum number of configurations to generate
+     * @return configurations for Tasks
+     */
+    @Override
+    public List<Map<String, String>> taskConfigs(int maxTasks) {
+        return null;
+    }
+
+    /**
+     * Stop this connector.
+     */
+    @Override
+    public void stop() {
+
+    }
+
+    /**
+     * Define the configuration for the connector.
+     *
+     * @return The ConfigDef for this connector; may not be null.
+     */
+    @Override
+    public ConfigDef config() {
+        return null;
+    }
+
+    /**
+     * Get the version of this component.
+     *
+     * @return the version, formatted as a String. The version may not be (@code null} or empty.
+     */
+    @Override
+    public String version() {
+        return version;
+    }
+}
---
+++ src/main/java/org/jlab/kafka/CASourceTask.java
@@ -0,0 +1,190 @@
+package org.jlab.kafka;
+
+import com.cosylab.epics.caj.CAJChannel;
+import com.cosylab.epics.caj.CAJContext;
+import com.cosylab.epics.caj.CAJMonitor;
+import gov.aps.jca.CAException;
+import gov.aps.jca.JCALibrary;
+import gov.aps.jca.Monitor;
+import gov.aps.jca.TimeoutException;
+import gov.aps.jca.configuration.DefaultConfiguration;
+import gov.aps.jca.dbr.DBR;
+import gov.aps.jca.event.MonitorEvent;
+import gov.aps.jca.event.MonitorListener;
+import org.apache.kafka.connect.connector.Connector;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.errors.ConnectException;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.apache.kafka.connect.source.SourceTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CASourceTask extends SourceTask {
+    private static final Schema VALUE_SCHEMA = Schema.STRING_SCHEMA;
+    private static final Logger log = LoggerFactory.getLogger(CASourceTask.class);
+    private static final JCALibrary JCA_LIBRARY = JCALibrary.getInstance();
+    private DefaultConfiguration config = new DefaultConfiguration("config");
+    private CAJContext context;
+    private List<String> pvs;
+    private Map<String, DBR> latest = new ConcurrentHashMap<>();
+
+    /**
+     * Get the version of this task. Usually this should be the same as the corresponding {@link Connector} class's version.
+     *
+     * @return the version, formatted as a String
+     */
+    @Override
+    public String version() {
+        return CAConnectorSource.version;
+    }
+
+    /**
+     * Start the Task
+     *
+     * @param props initial configuration
+     */
+    @Override
+    public void start(Map<String, String> props) {
+
+        String epicsAddrList = props.get("EPICS_CA_ADDR_LIST");
+        pvs = Arrays.asList(props.get("PVS_CSV").split(","));
+
+        config.setAttribute("class", JCALibrary.CHANNEL_ACCESS_JAVA);
+        config.setAttribute("auto_addr_list", "false");
+        config.setAttribute("addr_list", epicsAddrList);
+    }
+
+    /**
+     * <p>
+     * Poll this source task for new records. If no data is currently available, this method
+     * should block but return control to the caller regularly (by returning {@code null}) in
+     * order for the task to transition to the {@code PAUSED} state if requested to do so.
+     * </p>
+     * <p>
+     * The task will be {@link #stop() stopped} on a separate thread, and when that happens
+     * this method is expected to unblock, quickly finish up any remaining processing, and
+     * return.
+     * </p>
+     *
+     * @return a list of source records
+     */
+    @Override
+    public List<SourceRecord> poll() throws InterruptedException {
+        if(context == null) {
+            try {
+                context = (CAJContext) JCA_LIBRARY.createContext(config);
+
+                List<CAJChannel> channels = new ArrayList<>();
+                for(String pv: pvs) {
+                    channels.add((CAJChannel)context.createChannel(pv));
+            }
+
+                context.pendIO(2.0);
+
+                for(CAJChannel channel: channels) {
+                    CAJMonitor monitor = (CAJMonitor) channel.addMonitor(Monitor.VALUE);
+
+                    monitor.addMonitorListener(new MonitorListener() {
+                        @Override
+                        public void monitorChanged(MonitorEvent ev) {
+                            latest.put(channel.getName(), ev.getDBR());
+                        }
+                    });
+                }
+
+                context.pendIO(2.0);
+
+            } catch(CAException | TimeoutException e) {
+                log.error("Error while trying to create CAJContext");
+                throw new ConnectException(e);
+            }
+        }
+
+        synchronized (this) {
+            wait(1000);
+        }
+
+        ArrayList<SourceRecord> recordList = new ArrayList<>();
+
+        for(String pv: pvs) {
+            DBR record = latest.get(pv);
+            String value = dbrToString(record);
+            Instant timestamp = Instant.now();
+            String topic = pv;
+            recordList.add(new SourceRecord(offsetKey(pv), offsetValue(timestamp.toEpochMilli()), topic, null,
+                    null, null, VALUE_SCHEMA, value, System.currentTimeMillis()));
+        }
+
+        return recordList;
+    }
+
+    /**
+     * Stop this task.
+     */
+    @Override
+    public synchronized void stop() {
+        try {
+            context.destroy();
+        } catch(CAException e) {
+            log.error("Failed to destroy CAJContext", e);
+        }
+        notify();
+    }
+
+    private Map<String, String> offsetKey(String name) {
+        return Collections.singletonMap("PV_NAME", name);
+    }
+
+    private Map<String, Long> offsetValue(Long pos) {
+        return Collections.singletonMap("Timestamp", pos);
+    }
+
+    private String dbrToString(DBR dbr) {
+        String result = null;
+        try {
+            if (dbr.isDOUBLE()) {
+                double value = ((gov.aps.jca.dbr.DOUBLE) dbr).getDoubleValue()[0];
+                if (Double.isFinite(value)) {
+                    result = String.valueOf(value);
+                } else if (Double.isNaN(value)) {
+                    result = "NaN";
+                } else {
+                    result = "Infinity";
+                }
+            } else if (dbr.isFLOAT()) {
+                float value = ((gov.aps.jca.dbr.FLOAT) dbr).getFloatValue()[0];
+                if (Float.isFinite(value)) {
+                    result = String.valueOf(value);
+                } else if (Float.isNaN(value)) {
+                    result = "NaN";
+                } else {
+                    result = "Infinity";
+                }
+            } else if (dbr.isINT()) {
+                int value = ((gov.aps.jca.dbr.INT) dbr).getIntValue()[0];
+                result = String.valueOf(value);
+            } else if (dbr.isSHORT()) {
+                short value = ((gov.aps.jca.dbr.SHORT) dbr).getShortValue()[0];
+                result = String.valueOf(value);
+            } else if (dbr.isENUM()) {
+                short value = ((gov.aps.jca.dbr.ENUM) dbr).getEnumValue()[0];
+                result = String.valueOf(value);
+            } else if (dbr.isBYTE()) {
+                byte value = ((gov.aps.jca.dbr.BYTE) dbr).getByteValue()[0];
+                result = String.valueOf(value);
+            } else {
+                String value = ((gov.aps.jca.dbr.STRING) dbr).getStringValue()[0];
+                result = String.valueOf(value);
+            }
+        } catch (Exception e) {
+            System.err.println("Unable to create String from value: " + e);
+            dbr.printInfo();
+        }
+
+        return result;
+    }
+}
