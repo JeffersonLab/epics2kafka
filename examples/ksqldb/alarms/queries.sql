092d9bef808013e8cf2615effe318ef00332e55c
ryans <ryans@jlab.org>
2020-10-12 13:31:46 -0400

    Use registrations2epics for alarm example

--- examples/ksqldb/alarms/queries.sql
+++
@@ -1,5 +0,0 @@
-SET 'auto.offset.reset' = 'earliest';
-
-CREATE STREAM connectAlarms (channel VARCHAR KEY, severity VARCHAR) WITH (kafka_topic='connect-alarms', value_format='json', partitions=1);
-
-CREATE STREAM activeAlarms WITH (kafka_topic='active-alarms', VALUE_FORMAT='avro', PARTITIONS=1, REPLICAS=1) AS SELECT * FROM connectAlarms EMIT CHANGES;
