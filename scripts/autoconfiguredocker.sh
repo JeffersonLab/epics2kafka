305254e5470fa2afd24950816c6e322e8d4d84e4
ryans <ryans@jlab.org>
2020-11-16 17:14:11 -0500

    Attempting to quiet noisy logging from Jersey lib

--- scripts/autoconfiguredocker.sh
+++ scripts/autoconfiguredocker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# We need to configure java.util.logging option on JVM eventually launched by /kafka/bin/connect-distributed.sh
+# - all this just to quiet some noisy log messages from some third party dependency
+EXTRA_ARGS="-Djava.util.logging.config.file=/kafka/config/logging.properties"
+
 # Launch original container ENTRYPOINT in background
 /docker-entrypoint.sh start &
 
